9b6f6f75b037055cb70cb9f1910b58732db6b6d5
subrata chhat <subratachhat777@gmail.com>
2024-07-14 21:15:39 +0530

    create sign in api route

--- backend/Controllers/auth.controller.js
+++ backend/Controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -11,6 +12,27 @@ export const signup = async (req, res, next) => {
     res.status(201).json({ message: "User created successfully!" });
   } catch (error) {
     // next(errorHandler(550, "error from the function"));
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    // this will check for if the email is exists or not
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Wrong Credentials!"));
+    const token = jwt.sign({ id: validUser?._id }, process.env.JWT_SECRET);
+
+    // we don't want other or users to show their password after login so do this to hide the password
+    const { password: pass, ...restData } = validUser?._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(restData);
+  } catch (error) {
     next(error);
   }
 };
