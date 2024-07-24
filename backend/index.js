8bde4fa8927b70f02e785d4a1c0af46c3995df04
subrata chhat <subratachhat777@gmail.com>
2024-07-24 13:45:16 +0530

    add create listing api route

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./Routes/user.route.js";
 import authRouter from "./Routes/auth.route.js";
+import listingRouter from "./Routes/listing.route.js";
 import cookieParser from "cookie-parser";
 dotenv.config();
 
@@ -26,6 +27,7 @@ app.listen(3000, () => {
 // API ROUTES
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 
 // MIDDLEWARE FOR ERROR HANDLING
 app.use((err, req, res, next) => {
