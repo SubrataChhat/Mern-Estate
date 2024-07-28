cce0a0063fd33f12e9322f604d31621a4460dd03
subrata chhat <subratachhat777@gmail.com>
2024-07-28 16:15:30 +0530

    create update listing api route

--- backend/Routes/listing.route.js
+++ backend/Routes/listing.route.js
@@ -2,12 +2,14 @@ import express from "express";
 import {
   createListing,
   deleteListing,
+  updateListing,
 } from "../Controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, createListing);
+router.post("/update/:id", verifyToken, updateListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
 
 export default router;
